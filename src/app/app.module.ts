e05f88431c874f5b9f052893dcadd76a02cd9ef9
Miguel Á. Sastre <sastre113@gmail.com>
2023-05-24 22:49:27 +0200

    modal inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { DialogAnimationsExample } from "./modal/dialog-animations-example";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
     declarations: [
-        AppComponent,
-        HomeComponent
+        AppComponent
     ],
     providers: [],
     bootstrap: [AppComponent],
